ecc3912503bc575c9fd6b2157dd27b85b176ee6f
dominicroemer <dominicroemer@gmail.com>
2021-10-07 16:23:31 -0700

    Create sample of basic tile design and layout

--- script.js
+++ script.js
@@ -10,26 +10,38 @@ let myLibrary = [];
 // Object constructor to make book objects
 const Game = function() {
     let name;
-    let played;
+    let platform;
+    let owned;
+    let desireToPlay;
+    let icon;
+    let beat;
 }
 
-Game.prototype.init = function(name, played) {
+Game.prototype.init = function(name, platform, owned, desireToPlay, icon, beat){
     this.name = name;
-    this.played = played;
+    this.platform = platform;
+    this.owned = owned;
+    this.desireToPlay = desireToPlay;
+    this.icon = icon;
+    this.beat = beat;
     return this;
 }
 
 Game.prototype.readState = function() {
-    console.log(`Hello! I am ${this.name}, and I have ${this.played ? '' : 'not '}played this game.`);
+    console.log(`Hello! I am ${this.name}. I am on ${this.platform}, I ${this.owned ? 'own' : `don't own`} this game. I have a ${this.desireToPlay}/10 desire to play this game. My icon is ${this.icon}, and I have ${this.beat ? 'beat' : 'not beat'} this game.`);
+    console.table(this);
 }
 
-let destiny = Object.create(Game.prototype).init('Destiny 2', true);
+
+// Sample games to fill myLibrary[] for functionality testing AND for default games to load on empty browsers with no localStorage of their own
+let destiny = Object.create(Game.prototype).init('Destiny 2', 'Steam', true, 8, 'https://cdn.realsport101.com/images/ncavvykf/gfinityesports/156765e6c912ff3352a5c7e279cb425fb446baa3-1920x1080.jpg', true);
 destiny.readState();
 
-let doom = Object.create(Game.prototype).init('Doom Eternal', false);
+let doom = Object.create(Game.prototype).init('Doom Eternal', 'Steam', true, 9, '.', false);
 doom.readState();
 
-let monsterHunter = Object.create(Game.prototype).init('Monster Hunter: Rise', false);
+let monsterHunter = Object.create(Game.prototype).init('Monster Hunter: Rise', 'Switch', true, 9, '.', false);
+monsterHunter.readState();
 
 myLibrary[0] = destiny;
 myLibrary[1] = doom;
@@ -45,6 +57,12 @@ updateDisplay();
 function addBookToLibrary() {
 
 
+
+
+}
+
+function createGameElement( game ) {
+    
 
 
 }
