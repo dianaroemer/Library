127675435f4b138df611399eff3e8a2b1e0e68fb
dominicroemer <dominicroemer@gmail.com>
2021-10-08 16:14:29 -0700

    Removed commented tester lines to clean up code

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ myLibrary[2] = monsterHunter;
 
 // ----------------------------- Library Functions -----------------------------
 
-// This function take's a user's input and stores the new book objects into an array
+// This function take's a user's input and creates new game object, attaches gameInstance to myLibrary[], and appends the game object to shelf.
 function addBookToLibrary() {
 
     // Prompt user details for info about the game
@@ -87,7 +87,10 @@ function addBookToLibrary() {
     // let gameInstance = Object.create(Game.prototype).initGame(name, platform, owned, desireToPlay, icon, beat, div);
     // myLibrary[myLibrary.length] = gameInstance;
 
+
+
     // For Testing use
+    // Instantiate user inputs to variables
     let gameName = "Nutter Butter"
     let platform = "Steam"
     let owned = true;
@@ -95,72 +98,21 @@ function addBookToLibrary() {
     let icon = "http://images.ctfassets.net/rporu91m20dc/3xdOAVjV4Q8CeSmg4UcK4K/5c9eff913b6b2c6c06f667a234e1464e/DOOM_LargeHero_Announce.jpg";
     let beat = false;
 
-    
+    // Instantiate new Game Object via user inputs above
     let gameInstance = Object.create(Game.prototype).initGame(gameName, platform, owned, desireToPlay, icon, beat);
 
+    // Add reference to gameInstance to myLibrary for later read, update, destroy functions
     myLibrary[myLibrary.length] = gameInstance;
+
+    // Add gameInstance to DOM 
+    shelf.appendChild(gameInstance.div);
     
 }
 
 addBookToLibrary();
-shelf.appendChild(myLibrary[myLibrary.length -1 ].div);
-
-// myLibrary[3].div.innerHTML = myLibrary[3].name;
-
-// myLibrary[3].div.innerHTML = `<div class="platform-icon">
-// <img src="https://upload.wikimedia.org/wikipedia/commons/8/83/Steam_icon_logo.svg" alt="Steam Game Platform Logo" class="slot-platform-icon">
-// </div class="platform-icon">`;
-// myLibrary[3].div.innerHTML += `<img src="https://cdn.realsport101.com/images/ncavvykf/gfinityesports/156765e6c912ff3352a5c7e279cb425fb446baa3-1920x1080.jpg" alt="Destiny 2 Video Game Logo" class="slot-icon">`
-// myLibrary[3].div.innerHTML += `${myLibrary[3].name} <br>`;
-// myLibrary[3].div.innerHTML += `Owned: ${myLibrary[3].owned ? 'Yes' : 'No' } <br>`;
-// myLibrary[3].div.innerHTML += `Desire to Play: ${myLibrary[3].desireToPlay}/10 <br>`;
-// myLibrary[3].div.innerHTML += `Beat: ${myLibrary[3].beat ? "True" : "False" } <br>`;
-// myLibrary[3].div.innerHTML += `<button class="slot-button">Modify</button>`;
 
 
 
-
-function buildHTML (game) {
-
-    // This function takes a game object and builds the correct format of inner HTML to be parsed into a new slot object
-    let html;
-    // let html = '<div class = "platform-icon"> <br>';
-    // html += `<img src="${this.platform}" alt="Steam Game Platform Logo" class="slot-platform-icon"> <br>`;
-    // html += `</div> <br>`;
-    html += `${game.name} <br>`;
-
-
-    return html;
-
-
-//     <div class="platform-icon">
-//     <img src="https://upload.wikimedia.org/wikipedia/commons/8/83/Steam_icon_logo.svg" alt="Steam Game Platform Logo" class="slot-platform-icon">
-// </div>  
-
-// <img src="https://cdn.realsport101.com/images/ncavvykf/gfinityesports/156765e6c912ff3352a5c7e279cb425fb446baa3-1920x1080.jpg" alt="Destiny 2 Video Game Logo" class="slot-icon">
-// <br>
-// <b>Destiny 2</b>
-// <br>
-// Owned: Yes
-// <br>
-// Desire to Play: 7/10
-// <br>
-// Beat: False
-// <br>
-// <button class="slot-button">Modify</button>
-}
-
-
-
-
-
-
-// This function takes the game object and creates a relevant div, attaching it to the object for creation in the DOM
-function createGameDiv( game ) {
-    
-
-
-}
 
 // This function loops through myLibrary[] and displays each book on the page.
 function updateDisplay() {
@@ -169,46 +121,6 @@ function updateDisplay() {
     return console.log("updateDisplay completed its runtime & hit its return");
 
 }
-
-// console.log(shelf);
-
-// element.appendChild(child);
-
-let div = document.createElement('div');
-div.setAttribute('class', `slot`);
-// shelf.appendChild(div);
-// shelf.removeChild(div);
-
-// div.innerHTML = "What <br>";
-// div.innerHTML += "is up <br>";
-// div.innerHTML += "my dude";
-
-// div.innerHTML = buildHTML(myLibrary[3]);
-
-
-
-let refer = document.getElementsByClassName('slot');
-// console.log(refer[0].innerHTML);
-
-
-// shelf.removeChild(refer[1]);
-
-
-// myLibrary[0].div = document.createElement('div');
-// myLibrary[0].div.setAttribute('class', 'slot');
-
-// shelf.appendChild(myLibrary[0].div);
-
-// myLibrary[0].div.innerHTML = "Quack <br>"
-// myLibrary[0].div.innerHTML += "Quackers <br>"
-
-// let refer = document.getElementsByClassName('slot');
-// let myReferr = refer[3];
-
-
-// console.log(myLibrary[0].div.innerHTML);
-
-
 
 
 
