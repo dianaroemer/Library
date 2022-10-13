fde3c04db59af7171e75850008357b8ee322f4b5
D Roemer <dominicroemer@gmail.com>
2022-10-13 16:02:48 -0700

    Establish firebase testbed for adding and retrieving data from firebase store

--- library-react/src/App.js
+++ library-react/src/App.js
@@ -1,12 +1,88 @@
-import logo from './logo.svg';
+import React, { Component, useEffect, useState } from 'react';
 import './App.css';
 import NavBar from './Components/NavBar';
 
-function App() {
+import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore/lite'
+
+
+
+function App(props) {
+
+  const db = getFirestore(props.firebaseapp);
+
+  async function handleTestButtonClick(e){
+    e.preventDefault();
+    console.log(`you clicked the test button`);
+    try{
+      const docRef = await addDoc(collection(db, "testCollection"), {
+        firstName: "bugger",
+        lastName: "everything",
+        year: 2022
+      });
+      console.log('Document written with id: ', docRef.id);
+    } catch (e) {
+      console.log("Error adding document: ", e);
+    }
+  }
+
+  // const querySnapshot = await getDocs(collection(db, 'testCollection'));
+
+  const [querySnapshot, setQuerySnapshot] = useState(
+    async () => {await getDocs(collection(db, "testCollection"))}
+  );
+
+  
+
+  function handleTestButtonClick1(e){
+    e.preventDefault();
+    // querySnapshot.forEach((doc) => {
+    //   console.log(`${doc.id} => ${doc.data()}`);
+    // });
+    // console.log(querySnapshot);
+    // console.log(getTestCollection(db));
+    const newVals = getTestCollection(db);
+    console.log(newVals);
+
+    
+
+  }
+
+  async function getTestCollection(db){
+    const testCol = collection(db, 'testCollection');
+    const testSnapshot = await getDocs(testCol);
+    const contentList = testSnapshot.docs.map(doc => doc.data());
+    return contentList;
+  }
+
+  // async function establishQuerySnapshot(){
+
+  //   setQuerySnapshot(await getDocs(collection(db, "testCollection")))
+  //   querySnapshot.forEach((doc) => {
+  //     console.log(`${doc.id} => ${doc.data()}`);
+  //   });
+
+  // }
+
+  // useEffect( () => {
+  //   establishQuerySnapshot();
+  // }, [])
+
+  // establishQuerySnapshot();
+
+
   return (
     <div className="App">
-      <NavBar></NavBar>
-      <header className="App-header">
+      <NavBar/>
+      <div>
+        Shiiiyeeet
+        <button onClick={(e) => {
+          handleTestButtonClick(e);
+        }}>test button</button>
+        <button onClick={(e) => {
+          handleTestButtonClick1(e);
+        }}>test button 1</button>
+      </div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +95,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
--- library-react/src/index.js
+++ library-react/src/index.js
@@ -4,10 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCqGEULyIsrylNhOCmsZ0Zo5Hko1b6ZQuY",
+  authDomain: "vg-library-60168.firebaseapp.com",
+  projectId: "vg-library-60168",
+  storageBucket: "vg-library-60168.appspot.com",
+  messagingSenderId: "967188474927",
+  appId: "1:967188474927:web:f0e0e9b64faceac616eada",
+  measurementId: "G-ZC7S583ZGJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App firebaseapp={app}/>
   </React.StrictMode>
 );
 
