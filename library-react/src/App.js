298f985be97c5342dbfe52ffa7574b7fcf900f75
D Roemer <dominicroemer@gmail.com>
2022-10-21 16:01:11 -0700

    Abstract queryMenu into its own component, style queryMenuButtons

--- library-react/src/App.js
+++ library-react/src/App.js
@@ -3,6 +3,7 @@ import uniqid from 'uniqid';
 import './App.css';
 import NavBar from './Components/NavBar';
 import GameTile from './Components/GameTile';
+import QueryMenu from './Components/QueryMenu';
 
 // import {getFirestore, collection, getDocs, addDoc, serverTimestamp, query } from 'firebase/firestore/lite'
 import {doc, onSnapshot, getFirestore, getDocs, addDoc, serverTimestamp, query, collection} from "firebase/firestore"
@@ -86,15 +87,7 @@ function App(props) {
 
   })
 
-  const [desireToPlayQueryMenu, setDesireToPlayQueryMenu] = useState(5);
-  function handleDesireToPlayListener(e){
-    e.preventDefault();
-    console.log(`You have adjusted the desireToPlaySlider in queryMenu `, e)
-    setDesireToPlayQueryMenu(e.target.value)
-  }
-
-
-
+  
 
   return (
     <div className="App">
@@ -128,78 +121,7 @@ function App(props) {
           console.log('You clicked the addSlot button');
         }}> + </button>
 
-      <div className='queryMenu'>
-
-        <div className='queryMenuContainer'>
-
-          Name: 
-          <input className='queryMenuInput' 
-            value="What is this game's name?"
-            id='queryMenuName'/>
-          <br/>
-
-          Logo: 
-          <input type='url' 
-            className='queryMenuInput'
-            value="Link an image here!"
-            id='queryMenuIcon'/>
-          <br/>
-
-          Owned:
-          <input type='checkbox' 
-            className='queryMenuCheck'
-            id='queryMenuOwned'/>
-          <br/>
-
-          Desire to Play:
-          <input type='range' min='1' max="10" value={desireToPlayQueryMenu}
-            className='queryMenuSlider'
-            onInput={(e) => {
-              // e.preventDefault();
-              // console.log('you adjusted the desire to play slider')
-              handleDesireToPlayListener(e);
-            }}/>
-            <output>{desireToPlayQueryMenu}</output>
-            <br/>
-
-            Did you beat this game?: 
-            <input type='checkbox'
-              className='queryMenuCheck'
-              id='queryMenuBeat'/>
-            <br/>
-
-            Console:
-            <input type='radio' value='pc' 
-              className='queryMnuRadio'
-              name='console' defaultChecked/>
-              <label for="pc">PC</label>
-
-            <input type='radio' value='ps4' 
-              className='queryMnuRadio'
-              name='console'/>
-              <label for="pc">PS4</label>
-
-            <input type='radio' value='xbox' 
-              className='queryMnuRadio'
-              name='console'/>
-              <label for="pc">Xbox</label>
-
-            <input type='radio' value='switch' 
-              className='queryMnuRadio'
-              name='console'/>
-              <label for="pc">Switch</label>
-
-              <div className="queryButtons">
-                  <button className="queryCommitButton" id="querySaveButton"> Save </button>
-                  <button className="queryCommitButton" id="queryDeleteButton"> Delete </button>
-              </div>
-
-
-
-        </div>
-
-      </div>
-
+      <QueryMenu/>
 
 
       {/* <header className="App-header">
---
+++ library-react/src/Components/QueryMenu.js
@@ -0,0 +1,94 @@
+import React, {useState, useEffect} from 'react'
+import '../Styling/QueryMenu.css'
+
+function QueryMenu(props){
+
+const [desireToPlayQueryMenu, setDesireToPlayQueryMenu] = useState(5);
+function handleDesireToPlayListener(e){
+    e.preventDefault();
+    console.log(`You have adjusted the desireToPlaySlider in queryMenu `, e)
+    setDesireToPlayQueryMenu(e.target.value)
+}
+
+
+return (
+
+    <div className='queryMenu'>
+
+    <div className='queryMenuContainer'>
+
+      Name: 
+      <input className='queryMenuInput' 
+        value="What is this game's name?"
+        id='queryMenuName'/>
+      <br/>
+
+      Logo: 
+      <input type='url' 
+        className='queryMenuInput'
+        value="Link an image here!"
+        id='queryMenuIcon'/>
+      <br/>
+
+      Owned:
+      <input type='checkbox' 
+        className='queryMenuCheck'
+        id='queryMenuOwned'/>
+      <br/>
+
+      Desire to Play:
+      <input type='range' min='1' max="10" value={desireToPlayQueryMenu}
+        className='queryMenuSlider'
+        onInput={(e) => {
+          // e.preventDefault();
+          // console.log('you adjusted the desire to play slider')
+          handleDesireToPlayListener(e);
+        }}/>
+        <output>{desireToPlayQueryMenu}</output>
+        <br/>
+
+        Did you beat this game?: 
+        <input type='checkbox'
+          className='queryMenuCheck'
+          id='queryMenuBeat'/>
+        <br/>
+
+        Console:
+        <input type='radio' value='pc' 
+          className='queryMnuRadio'
+          name='console' defaultChecked/>
+          <label htmlFor='pc'>PC</label>
+
+        <input type='radio' value='ps4' 
+          className='queryMnuRadio'
+          name='console'/>
+          <label htmlFor='pc'>PS4</label>
+
+        <input type='radio' value='xbox' 
+          className='queryMnuRadio'
+          name='console'/>
+          <label htmlFor='pc'>Xbox</label>
+
+        <input type='radio' value='switch' 
+          className='queryMnuRadio'
+          name='console'/>
+          <label htmlFor='pc'>Switch</label>
+
+          <div className="queryButtons">
+              <button className="queryCommitButton" id="querySaveButton"> Save </button>
+              <button className="queryCommitButton" id="queryDeleteButton"> Delete </button>
+          </div>
+
+
+
+    </div>
+
+  </div>
+
+
+
+)
+
+}
+
+export default QueryMenu;
